1dae297fc7a3d9c399cf35231ab13498b8f36c68
Avish Kumar <80064807+avishmonga@users.noreply.github.com>
2022-04-13 19:51:06 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const users = require("./data/users");
 const movies = require("./data/movie");
+const appointment = require("./data/appointment")
 module.exports = {
   users,
-  movies
+  movies,
+  appointment
 }
