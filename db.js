0d6c6a2c05290b4be8b7eb0ca225f35b998ef525
Avish Kumar <80064807+avishmonga@users.noreply.github.com>
2022-09-11 17:35:25 +0530

    watches data addded

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const users = require("./data/users");
 const movies = require("./data/movie");
 const appointment = require("./data/appointment")
+const watches = require("./data/watch.json");
 module.exports = {
   users,
   movies,
-  appointment
+  appointment,
+  watches
 }
